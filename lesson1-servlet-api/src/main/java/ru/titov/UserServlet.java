d3f46d2873852342505a705d6b57d720c50151bb
VladimirTitov4 <vtitov4@yandex.ru>
2022-08-08 21:59:14 +0300

    Материалы первого урока

---
+++ lesson1-servlet-api/src/main/java/ru/titov/UserServlet.java
@@ -0,0 +1,50 @@
+package ru.titov;
+
+import ru.titov.persist.User;
+import ru.titov.persist.UserRepository;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = "/user/*")
+public class UserServlet extends HttpServlet {
+
+    private UserRepository userRepository;
+
+    @Override
+    public void init() throws ServletException {
+        this.userRepository = new UserRepository();
+        this.userRepository.insert(new User("Vladimir"));
+        this.userRepository.insert(new User("Anastasia"));
+        this.userRepository.insert(new User("Evgeniy"));
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        /*String pathInfo = request.getPathInfo();
+        String contextPath = request.getContextPath();
+        response.getWriter().println("request.getPathInfo() " + "<p>" + pathInfo + "</p>");
+        response.getWriter().println("request.getContextPath() " + "<p>" + contextPath + "</p>");*/
+
+        PrintWriter writer = response.getWriter();
+        writer.println("<table>");
+        writer.println("<tr>");
+        writer.println("<th>id</th>");
+        writer.println("<th>username</th>");
+        writer.println("</tr>");
+
+        for (User user : userRepository.findAll()) {
+            writer.println("<tr>");
+            writer.println("<td>" + user.getId() + "</td>");
+            writer.println("<td>" + user.getUsername() + "</td>");
+            writer.println("</tr>");
+        }
+
+        writer.println("</table>");
+    }
+}
