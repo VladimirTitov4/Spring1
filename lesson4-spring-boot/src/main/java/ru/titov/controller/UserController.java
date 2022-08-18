f53ba1bfe78ef2070ce320f97710ce73df1a69dd
vladimir <vtitov4@yandex.ru>
2022-08-18 21:17:39 +0300

    Базовая настройка проекта

---
+++ lesson4-spring-boot/src/main/java/ru/titov/controller/UserController.java
@@ -0,0 +1,60 @@
+package ru.titov.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.titov.persist.User;
+import ru.titov.persist.UserRepository;
+
+@Slf4j
+@Controller
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    @GetMapping
+    public String listPage(Model model) {
+        model.addAttribute("users", userRepository.findAll());
+        return "user";
+    }
+
+    @GetMapping("/{id}")
+    public String form(@PathVariable("id") long id, Model model) {
+        model.addAttribute("user", userRepository.findById(id));
+        return "user_form";
+    }
+
+    @GetMapping("/new")
+    public String addNewUser(Model model) {
+        model.addAttribute("user", new User(""));
+        return "user_form";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteUserById(@PathVariable long id) {
+        userRepository.delete(id);
+        return "redirect:/user";
+    }
+
+    @PostMapping
+    public String saveUser(User user) {
+        log.info("method saveUser was hit");
+        userRepository.save(user);
+        return "redirect:/user";
+    }
+
+    @PostMapping("/update")
+    public String updateUser(User user) {
+        userRepository.save(user);
+        return "redirect:/user";
+    }
+
+
+}
