99812e5bfd30c9f5bc2733d36e7ba800c7a0b7f9
vladimir <vtitov4@yandex.ru>
2022-08-30 20:54:56 +0300

    ДЗ 6-го урока.

---
+++ lesson5-hibernate/src/main/java/ru/titov/model/Product.java
@@ -0,0 +1,46 @@
+package ru.titov.model;
+
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(columnDefinition = "text")
+    private String description;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    @OneToMany(mappedBy = "product")
+    private List<LineItem> lineItems;
+
+    public Product(String name, String description, BigDecimal price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+}
